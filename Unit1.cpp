c5e8f9d0e30197a2abcdb5044243c3cff4e5af0d
Karol Sołek <karol.solek.programista@gmail.com>
2019-03-09 19:48:32 +0100

    paddles-centre&paddles-moving_boosts

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,17 +4,22 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include <cmath>
 
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
-int v_x = 3;
-int v_y = -3;
+int v_x = +4;
+int v_y = -4;
+bool leftUpBoost = 0;
+bool leftDownBoost = 0;
+bool rightUpBoost = 0;
+bool rightDownBoost = 0;
 
 void corner_hit() {
-        v_x = -v_x;
         v_y = -v_y;
+        v_x = (-v_x/v_x)*abs(v_y);
 }
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
@@ -37,19 +42,43 @@ void __fastcall TForm1::paddle1downTimer(TObject *Sender)
 void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
       TShiftState Shift)
 {
-        if (Key == 'A') paddle1up -> Enabled = true;
-        if (Key == 'Z') paddle1down -> Enabled = true;
-        if (Key == VK_UP) paddle2up -> Enabled = true;
-        if (Key == VK_DOWN) paddle2down -> Enabled = true;
+        if (Key == 'A') {
+                paddle1up -> Enabled = true;
+                leftUpBoost = true;
+        }
+        if (Key == 'Z') {
+                paddle1down -> Enabled = true;
+                leftDownBoost = true;
+        }
+        if (Key == VK_UP) {
+                paddle2up -> Enabled = true;
+                rightUpBoost = true;
+        }
+        if (Key == VK_DOWN) {
+                paddle2down -> Enabled = true;
+                rightDownBoost = true;
+        }
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
       TShiftState Shift)
 {
-        if (Key == 'A') paddle1up -> Enabled = false;
-        if (Key == 'Z') paddle1down -> Enabled = false;
-        if (Key == VK_UP) paddle2up -> Enabled = false;
-        if (Key == VK_DOWN) paddle2down -> Enabled = false;
+        if (Key == 'A') {
+                paddle1up -> Enabled = false;
+                leftUpBoost = false;
+        }
+        if (Key == 'Z') {
+                paddle1down -> Enabled = false;
+                leftDownBoost = false;
+        }
+        if (Key == VK_UP) {
+                paddle2up -> Enabled = false;
+                rightUpBoost = false;
+        }
+        if (Key == VK_DOWN) {
+                paddle2down -> Enabled = false;
+                rightDownBoost = false;
+        }
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::paddle2upTimer(TObject *Sender)
@@ -64,40 +93,82 @@ void __fastcall TForm1::paddle2downTimer(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TForm1::ball_movingTimer(TObject *Sender)
 {
-        int ball_radius = ball->Width/2;
-        int ball_center_x = ball->Top + ball_radius;
-        //int ball_center_y = ball->Left + ball_radius;
+        int ball_center_x = ball->Top + ball->Width/2;
+        int paddle1_centre_x = paddle1->Top + paddle1->Height/2;
+        int paddle2_centre_x = paddle2->Top + paddle2->Height/2;
 
+        //odbicie od gornej i dolnej sciany
         if ((ball->Top <= background->Top) || (ball->Top >= background->Top + background->Height - ball->Height)) {
                 v_y = -v_y;
         }
+
+        //odbicie od lewej paletki
         if (ball->Left <= paddle1->Left+paddle1->Width) {
+
+                //boost na srodku paletki
+                if (abs(ball_center_x - paddle1_centre_x) < 50) {
+                        v_x -=1;
+                }
+                if (leftUpBoost) v_y -= 1;
+                if (leftDownBoost) v_y += 1;
+
                 if((ball_center_x >= paddle1->Top)
                 && (ball_center_x <= paddle1->Top+paddle1->Height)) {
                         v_x = -v_x;
-                } else if ((ball_center_x + ball->Height/2 >= paddle1->Top)
-                           && (ball_center_x <= paddle1->Top) && v_y > 0) {
+                } else if ((ball_center_x + ball->Height/2 >= paddle1->Top) //odbicie od gornego rogu
+                           && (ball_center_x <= paddle1->Top)
+                           && v_y > 0) {
                         corner_hit();
-                } else if ((ball_center_x >= paddle1->Top + paddle1->Height)
-                           && (ball->Top <= paddle1->Top + paddle1->Height + ball->Height) && v_y < 0) {
+                } else if ((ball_center_x + ball->Height/2 >= paddle1->Top) //odbicie od gornego rogu
+                           && (ball_center_x <= paddle1->Top)
+                           && v_y < 0) {
+                        v_x = -v_x;
+                } else if ((ball_center_x >= paddle1->Top + paddle1->Height) //odbicie od dolnego rogu
+                           && (ball_center_x <= paddle1->Top + paddle1->Height + ball->Height/2)
+                           && v_y < 0) {
                         corner_hit();
-                } else {
+                } else if ((ball_center_x >= paddle1->Top + paddle1->Height) //odbicie od dolnego rogu
+                           && (ball_center_x <= paddle1->Top + paddle1->Height + ball->Height/2)
+                           && v_y > 0) {
+                        v_x = -v_x;
+                } else { //skucie
                         ball_moving->Enabled = false;
+                        ball->Visible = false;
                 }
         }
 
-        if (ball->Left >= paddle2->Left-paddle2->Width) {
-                if((ball_center_x >= paddle2->Top )
+        //odbicie od prawej paletki
+        if (ball->Left + ball->Width >= paddle2->Left) {
+                if (abs(ball_center_x - paddle2_centre_x) < 50) {
+                        v_x +=1;
+                }
+                if (rightUpBoost) v_y -= 1;
+                if (rightDownBoost) v_y += 1;
+
+
+                if((ball_center_x >= paddle2->Top ) //
                 && (ball_center_x <= paddle2->Top+paddle2->Height)) {
                         v_x = -v_x;
-                }  else if ((ball_center_x + ball->Height/2 >= paddle2->Top)
-                           && (ball_center_x <= paddle2->Top) && v_y > 0) {
+                } else if ((ball_center_x + ball->Height/2 >= paddle2->Top)
+                           && (ball_center_x <= paddle2->Top)
+                           && v_y > 0) {
+                        corner_hit();
+                } else if ((ball_center_x + ball->Height/2 >= paddle2->Top)
+                           && (ball_center_x <= paddle2->Top)
+                           && v_y < 0) {
+                        v_x = -v_x;
+                } else if ((ball_center_x >= paddle2->Top + paddle2->Height)
+                           && (ball_center_x <= paddle2->Top + paddle2->Height + ball->Height/2)
+                           && v_y < 0) {
                         corner_hit();
                 } else if ((ball_center_x >= paddle2->Top + paddle2->Height)
-                           && (ball->Top <= paddle2->Top + paddle2->Height + ball->Height) && v_y < 0) {
-                        corner_hit();
+                           && (ball_center_x <= paddle2->Top + paddle2->Height + ball->Height/2)
+                           && v_y > 0) {
+                        v_x = -v_x;
                 } else {
                         ball_moving->Enabled = false;
+                        ball->Visible = false;
+                        return;
                 }
         }
         ball -> Left += v_x;
